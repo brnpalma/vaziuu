6b17b25d9d177f27a06118cdf3f989c71d284c7c
Bruno Oliveira <br88@outlook.com>
2025-10-15 19:54:11 -0300

    inicio do formulario

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Botao from "../Botao";
+class Formulario extends React.Component {
+    render(){
+        return <form>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+            <div><Botao/></div>
+        </form>
+    }
+}
